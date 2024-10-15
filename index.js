39aa2b8eb06da18bed3579eae22859f255fd72f6
Prashant <prashantrathore1435@gmail.com>
2024-10-15 23:06:16 +0530

    Ligth mode activated

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function toggleMenu() {
     overlay.classList.remove("show");
   });
 }
+const themeToggle = document.querySelector(".theme-toggle");
+const body = document.querySelector("body");
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+});
