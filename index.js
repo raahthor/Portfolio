615c9b3f1522c261dc3678a249376ef9e57c7d59
Prashant <prashantrathore1435@gmail.com>
2024-12-05 19:45:33 +0530

    sidebar upper sections updated

--- index.js
+++ index.js
@@ -43,18 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// const clickedItems = document.querySelectorAll(".side-bar ul li a");
-// for (let i = 0; i < clickedItems.length; i++) {
-//   clickedItems[i].addEventListener("click", function () {
-//     for (let j = 0; j < clickedItems.length; j++) {
-//       clickedItems[j].classList.remove("clicked");
-//     }
-//     this.classList.add("clicked");
-//     sidebar.classList.remove("open");
-//     overlay.classList.remove("show");
-//   });
-// }
-
 // for profile icon
 
 const pfpopen = document.querySelector("body .pfp");
